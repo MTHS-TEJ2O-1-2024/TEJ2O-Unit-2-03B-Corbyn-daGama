a8f217e625b410346221d92cda07d362588f18b4
Corbyn-daGama <corbyn.da-gama@mths.ca>
2024-09-17 11:20:07 -0400

    Added Calculations

--- main.ts
+++ main.ts
@@ -5,4 +5,18 @@
  * This program ... shows basic math
 */
 
-basic.showString('Hello, World!')
+
+
+
+basic.clearScreen()
+basic.pause(1000)
+
+
+basic.showString('a = l x w')
+basic.showString('a = 5*3' + (5*3))
+basic.pause(100)
+basic.showString('p = 2(l x w)')
+basic.pause(100)
+basic.showString('p = 2(5 * 3)')
+basic.pause(100)
+basic.showString ('p = ' + (2 *(5 * 3)).toString())
